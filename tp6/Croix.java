92b4337bf76c33a7eeae50ba44dbe5fc61f7a2c3
Donovane Houte <94303790+donovaneHoute@users.noreply.github.com>
2025-10-09 22:42:52 +0200

    Update Croix.java

--- tp6/Croix.java
+++ tp6/Croix.java
@@ -2,15 +2,27 @@ class Croix extends Program {
     
     String dessineDiagonale(int taille, char caractere) {
         String resultat = "";
-        for (int k = 1; k < taille; k++) {
-            for (int j = 1; j < k; j++) {
-                resultat += " ";
+        for (int i = 1; i <= taille; i++) {
+            String ligne = "";
+            for (int j = 1; j <= taille; j++) {
+                if (j == i || j == (taille - i + 1)) {
+                    ligne += caractere;
+                } else {
+                    ligne += " ";
+                }
             }
-            resultat += " \n";
-        }
+            
+            int k = length(ligne) - 1;
 
+            while (k >= 0 && charAt(ligne, k) == ' ') {
+                k--;
+            }
+            
+            resultat += substring(ligne, 0, k + 1) + "\n";
+        }
         return resultat;
     }
+
 
     void algorithm() {
         print("Taille : ");
