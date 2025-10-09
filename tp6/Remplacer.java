faa1d2c065c5f3e8d2e266daa00a9eaf16cb0efe
Donovane Houte <94303790+donovaneHoute@users.noreply.github.com>
2025-10-09 22:43:18 +0200

    Update Remplacer.java

--- tp6/Remplacer.java
+++ tp6/Remplacer.java
@@ -1,19 +1,5 @@
 class Remplacer extends Program {
-
-    String copieEnRemplacant(String phrase, String avant, String après) {
-        String resultat = "";
-
-        for (int idx = 0; idx < length(phrase); idx++) {
-            if (idx < length(phrase) - length(avant)) {
-                if (equals(substring(phrase, idx, idx + length(avant)), avant)) {
-                    resultat += après;
-                }
-            }
-        }
-
-        return resultat;
-    }
-
+    
     void testCopieEnRemplacant () {
         assertEquals("15x35"  , copieEnRemplacant("15*35"     , "*"   , "x"));
         assertEquals("15 + 35", copieEnRemplacant("15 plus 35", "plus", "+"));
